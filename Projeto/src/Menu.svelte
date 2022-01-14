4cc36d4a69f02db0d07c52b01e074253f885b061
jucielefernandes <juciele.bol@gmail.com>
2022-01-14 00:40:20 -0300

    ajustando commit #2

--- Projeto/src/Menu.svelte
+++ Projeto/src/Menu.svelte
@@ -1,45 +1,45 @@
-<svelte:head>
-	<link rel="stylesheet" href="public/buiild">
-</svelte:head>
-
-
-
 <script>
-	import { estado } from './Estado.js'
-	import { trocarEstadoDoJogo } from './Estado.js'
-</script>
-
-<h1 class = "titulo">
-    Jogo da memória
-</h1>
-
-<button class = "butao" on:click={() => trocarEstadoDoJogo('jogar')}>
-	Jogar
-</button>
-
-<button on:click={() => trocarEstadoDoJogo('sobre')}>
-	Sobre
-</button>
-
-<button on:click={() => trocarEstadoDoJogo('ajuda')}>
-	Ajuda
-</button>
-
-
-<style>
-    h1{
-        text-align: center;
-        border:solid;
-        border-style: double;
-        border-color: indigo;
-        color:royalblue;
+    import { estado } from "./Estado.js";
+    import { trocarEstadoDoJogo } from "./Estado.js";
+  </script>
+  
+  <svelte:head>
+    <link rel="stylesheet" href="public/buiild" />
+  </svelte:head>
+  
+  
+    <div class="botoes">
+      <button class="butao" on:click={() => trocarEstadoDoJogo("jogar")}>
+        Jogar
+      </button>
+  
+      <button on:click={() => trocarEstadoDoJogo("sobre")}> Sobre </button>
+  
+      <button on:click={() => trocarEstadoDoJogo("ajuda")}> Ajuda </button>
+    </div>
+  
+  
+  <style>
+    div.botoes{
+      align-items: center;
+      margin: 100px ;
     }
-
+   
     button{
-       color:royalblue;
-       cursor: pointer;
-       display: flex;
-       padding: 10px;
-       margin: auto;
+      border-radius: 0.6em;
+    border-color: royalblue;
+    color: #fff;
+    background-image: -webkit-linear-gradient(left, rgb(0, 64, 131), rgb(0, 64, 131) 5%, #0074bd 5%, #0074bd 10%, rgb(0, 64, 131) 10%, rgb(0, 64, 131) 15%, #0074bd 15%, #0074bd 20%, rgb(0, 64, 131) 20%, rgb(0, 64, 131) 25%, #0074bd 25%, #0074bd 30%, rgb(0, 64, 131) 30%, rgb(0, 64, 131) 35%, #0074bd 35%, #0074bd 40%, rgb(0, 64, 131) 40%, rgb(0, 64, 131) 45%, #0074bd 45%, #0074bd 50%, rgb(0, 64, 131) 50%, rgb(0, 64, 131) 55%, #0074bd 55%, #0074bd 60%, rgb(0, 64, 131) 60%, rgb(0, 64, 131) 65%, #0074bd 65%, #0074bd 70%, rgb(0, 64, 131) 70%, rgb(0, 64, 131) 75%, #0074bd 75%, #0074bd 80%, rgb(0, 64, 131) 80%, rgb(0, 64, 131) 85%, #0074bd 85%, #0074bd 90%, rgb(0, 64, 131) 90%, rgb(0, 64, 131) 95%, #0074bd 95%, #0074bd 100%);
+    background-image: linear-gradient(to right, rgb(0, 64, 131), rgb(0, 64, 131) 5%, #0074bd 5%, #0074bd 10%, rgb(0, 64, 131) 10%, rgb(0, 64, 131) 15%, #0074bd 15%, #0074bd 20%, rgb(0, 64, 131) 20%, rgb(0, 64, 131) 25%, #0074bd 25%, #0074bd 30%, rgb(0, 64, 131) 30%, rgb(0, 64, 131) 35%, #0074bd 35%, #0074bd 40%, rgb(0, 64, 131) 40%, rgb(0, 64, 131) 45%, #0074bd 45%, #0074bd 50%, rgb(0, 64, 131) 50%, rgb(0, 64, 131) 55%, #0074bd 55%, #0074bd 60%, rgb(0, 64, 131) 60%, rgb(0, 64, 131) 65%, #0074bd 65%, #0074bd 70%, rgb(0, 64, 131) 70%, rgb(0, 64, 131) 75%, #0074bd 75%, #0074bd 80%, rgb(0, 64, 131) 80%, rgb(0, 64, 131) 85%, #0074bd 85%, #0074bd 90%, rgb(0, 64, 131) 90%, rgb(0, 64, 131) 95%, #0074bd 95%, #0074bd 100%);
+    background-position: 0 0;
+    background-size: 100%;
+    -webkit-transition: background 300ms ease-in-out;
+    transition: background 300ms ease-in-out;
+    display: flex;
+    padding: 30px;
+    margin: auto;
     }
-</style>
+  button:hover {
+    background-position: 100px;
+  }
+  </style>
