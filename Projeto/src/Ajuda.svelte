0b7423a13962a8eb654db715efbd580bea914e16
jucielefernandes <juciele.bol@gmail.com>
2022-01-17 18:47:39 -0300

    Ajustando O Sobre

--- Projeto/src/Ajuda.svelte
+++ Projeto/src/Ajuda.svelte
@@ -1,15 +1,32 @@
 <script>
-	import VoltarMenu from './VoltarMenu.svelte'
+  import VoltarMenu from "./VoltarMenu.svelte";
 </script>
 
+<nav class="caixa">
+  <h1 class="titulo">Sobre o jogo</h1>
+  <h2>Esse jogo foi criado pelos alunos Juciele Fernandes, Adryan do curso Tecnologo em Sistemas para Internet no IFPE campus Igarassu na disciplina Logica administrada pelo professor Allan Diego  </h2>
+  <div>
+    <h1 class="titulo">Como Jogar</h1>
+    <h2>
+      Escolha duas cartas para desvirá-las. Caso formem um par, seus pontos são
+      acrecentados no jogo. Caso contrário as cartas voltam a ficar com as
+      figuras voltadas para baixo; O jogo termina quando todos os pares forem
+      formados ou a quantidade de erros forem superiores a 10.
+    </h2>
+  </div>
+</nav>
 
+<VoltarMenu />
 
+<style>
+  h2 {
+    color: aliceblue;
+  }
 
-<h1>Ajuda</h1>
-
-<p>Pesquisa no <a href = "www.google.com">Google</a>!!!</p>
-
-
-
-
-<VoltarMenu/>
+  .caixa {
+    width: 300px;
+    padding: 25px;
+    border: 25px solid navy;
+    margin: 25px;
+  }
+</style>
--- Projeto/src/Jogar.svelte
+++ Projeto/src/Jogar.svelte
@@ -1,10 +1,5 @@
-
 <script>
   import VoltarMenu from "./VoltarMenu.svelte";
-  import { estado } from "./Estado.js";
-  import { trocarEstadoDoJogo } from "./Estado.js";
-
-
 
   class EstadoTabela {
     constructor(limiteVerdadeiros, tabela) {
@@ -14,33 +9,47 @@
     }
   }
 
-  
-
-  
   let tabela = [
-    ['https://picsum.photos/id/237/536/354', 'https://picsum.photos/id/237/536/354', "https://picsum.photos/id/237/536/354","https://picsum.photos/id/237/536/354"],
-    ['https://picsum.photos/id/237/536/354', 'https://picsum.photos/id/237/536/354',"https://picsum.photos/id/237/536/354","https://picsum.photos/id/237/536/354"],
-    ['https://picsum.photos/id/237/536/354', 'https://picsum.photos/id/237/536/354',"https://picsum.photos/id/237/536/354","https://picsum.photos/id/237/536/354"],
+    [
+      "https://picsum.photos/id/237/536/354",
+      "https://picsum.photos/id/237/536/354",
+      "https://picsum.photos/id/237/536/354",
+      "https://picsum.photos/id/237/536/354",
+    ],
+    [
+      "https://picsum.photos/id/237/536/354",
+      "https://picsum.photos/id/237/536/354",
+      "https://picsum.photos/id/237/536/354",
+      "https://picsum.photos/id/237/536/354",
+    ],
+    [
+      "https://picsum.photos/id/237/536/354",
+      "https://picsum.photos/id/237/536/354",
+      "https://picsum.photos/id/237/536/354",
+      "https://picsum.photos/id/237/536/354",
+    ],
   ];
 
   let cartas = [
-    [ "https://i.picsum.photos/id/292/536/354.jpg?hmac=JmwZG4JsRmarXfsRwZuzcaOYhm5ZhvdpGAoX6a-syQ0" , "https://i.picsum.photos/id/44/536/354.jpg?hmac=gr9s90YEHTR2r0xSI_Q-BzX07et5YTXdnM0uRl9gYIc","https://i.picsum.photos/id/866/536/354.jpg?hmac=tGofDTV7tl2rprappPzKFiZ9vDh5MKj39oa2D--gqhA","https://i.picsum.photos/id/1060/536/354.jpg?blur=2&hmac=0zJLs1ar00sBbW5Ahd_4zA6pgZqCVavwuHToO6VtcYY"],
     [
-     "https://i.picsum.photos/id/292/536/354.jpg?hmac=JmwZG4JsRmarXfsRwZuzcaOYhm5ZhvdpGAoX6a-syQ0",
-      "https://picsum.photos/id/1084/536/354","https://i.picsum.photos/id/1060/536/354.jpg?blur=2&hmac=0zJLs1ar00sBbW5Ahd_4zA6pgZqCVavwuHToO6VtcYY",
-      "https://i.picsum.photos/id/870/536/354.jpg?blur=2&grayscale&hmac=A5T7lnprlMMlQ18KQcVMi3b7Bwa1Qq5YJFp8LSudZ84"
+      "https://i.picsum.photos/id/292/536/354.jpg?hmac=JmwZG4JsRmarXfsRwZuzcaOYhm5ZhvdpGAoX6a-syQ0",
+      "https://i.picsum.photos/id/44/536/354.jpg?hmac=gr9s90YEHTR2r0xSI_Q-BzX07et5YTXdnM0uRl9gYIc",
+      "https://i.picsum.photos/id/866/536/354.jpg?hmac=tGofDTV7tl2rprappPzKFiZ9vDh5MKj39oa2D--gqhA",
+      "https://i.picsum.photos/id/1060/536/354.jpg?blur=2&hmac=0zJLs1ar00sBbW5Ahd_4zA6pgZqCVavwuHToO6VtcYY",
+    ],
+    [
+      "https://i.picsum.photos/id/292/536/354.jpg?hmac=JmwZG4JsRmarXfsRwZuzcaOYhm5ZhvdpGAoX6a-syQ0",
+      "https://picsum.photos/id/1084/536/354",
+      "https://i.picsum.photos/id/1060/536/354.jpg?blur=2&hmac=0zJLs1ar00sBbW5Ahd_4zA6pgZqCVavwuHToO6VtcYY",
+      "https://i.picsum.photos/id/870/536/354.jpg?blur=2&grayscale&hmac=A5T7lnprlMMlQ18KQcVMi3b7Bwa1Qq5YJFp8LSudZ84",
     ],
     [
       "https://picsum.photos/id/1084/536/354",
       "https://i.picsum.photos/id/44/536/354.jpg?hmac=gr9s90YEHTR2r0xSI_Q-BzX07et5YTXdnM0uRl9gYIc",
       "https://i.picsum.photos/id/866/536/354.jpg?hmac=tGofDTV7tl2rprappPzKFiZ9vDh5MKj39oa2D--gqhA",
-      "https://i.picsum.photos/id/870/536/354.jpg?blur=2&grayscale&hmac=A5T7lnprlMMlQ18KQcVMi3b7Bwa1Qq5YJFp8LSudZ84"
+      "https://i.picsum.photos/id/870/536/354.jpg?blur=2&grayscale&hmac=A5T7lnprlMMlQ18KQcVMi3b7Bwa1Qq5YJFp8LSudZ84",
     ],
   ];
-
-
-  
- 
 
   let verso = "https://picsum.photos/id/237/536/354";
 
@@ -50,39 +59,35 @@
   let escolha1;
   let escolha2;
 
-  
   let estadoTabela = new EstadoTabela(6, tabela);
-
 
   let pontos = 0;
   let erros = 0;
 
-
   function shuffle(array) {
-  let currentIndex = array.length,  randomIndex;
+    let currentIndex = array.length,
+      randomIndex;
 
-  
-  while (currentIndex != 0) {
+    while (currentIndex != 0) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
 
-    
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex--;
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex],
+        array[currentIndex],
+      ];
+    }
 
-    
-    [array[currentIndex], array[randomIndex]] = [
-      array[randomIndex], array[currentIndex]];
+    return array;
+
+    //Função de embaralhar array:https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
   }
 
-  return array;
+  cartas = [shuffle(cartas[0]), shuffle(cartas[1]), shuffle(cartas[2])];
+  cartas = shuffle(cartas);
 
-  //Função de embaralhar array:https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
-}
-
-cartas = [shuffle(cartas[0]),shuffle(cartas[1]),shuffle(cartas[2])];
-cartas = shuffle(cartas)
-  
   function computarVerdadeiros(tabela) {
-    let verdadeiros=0
+    let verdadeiros = 0;
 
     for (let i = 0; i < tabela.length; i++) {
       for (let j = 0; j < tabela.length; j++) {
@@ -95,89 +100,68 @@ cartas = shuffle(cartas)
     return verdadeiros;
   }
 
-  function desflip(x,y,i,j){
-    setTimeout(function(){
-      
-      
+  function desflip(x, y, i, j) {
+    setTimeout(function () {
       estadoTabela.tabela[i][j] = verso;
       estadoTabela.tabela[x][y] = verso;
-    },1000);
+    }, 1000);
 
     return;
-
   }
 
-  function reset(){
-    escolha1 = null
-    escolha2=null
-    
+  function reset() {
+    escolha1 = null;
+    escolha2 = null;
+
     return;
   }
-  
-  
-  function flip (i,j){
-    
 
-      estadoTabela.tabela[i][j] = cartas[i][j];
+  function flip(i, j) {
+    estadoTabela.tabela[i][j] = cartas[i][j];
 
-      if(escolha2==null ){
-        x = i;
-        y = j
-        escolha2=cartas[x][y]
-        
+    if (escolha2 == null) {
+      x = i;
+      y = j;
+      escolha2 = cartas[x][y];
+    } else if (escolha1 == null) {
+      escolha1 = cartas[i][j];
+    }
 
-      }else if(escolha1==null){
-        
-        escolha1 = cartas[i][j]
-       
-      }
-      
- 
-      
-      if(escolha1==escolha2 && escolha1!=null && escolha2!=null){
-
-        igual(escolha1,escolha2,x,y,i,j)
-
-      }else if (escolha1!=escolha2 && escolha1!=null && escolha2!=null){
-
-        diferente(escolha1,escolha2,x,y,i,j)
-      }
-      
+    if (escolha1 == escolha2 && escolha1 != null && escolha2 != null) {
+      igual(escolha1, escolha2, x, y, i, j);
+    } else if (escolha1 != escolha2 && escolha1 != null && escolha2 != null) {
+      diferente(escolha1, escolha2, x, y, i, j);
+    }
   }
 
-
-  function igual(escolha1,escolha2){
-    
-
-      if(escolha2===escolha1){
-        
-        
-        
-        reset(escolha1,escolha2)
-
-       
-      }pontos++
-
-      
- 
-    
+  function igual(escolha1, escolha2) {
+    if (escolha2 === escolha1) {
+      reset(escolha1, escolha2);
+      pontos++;
+    }
+    alert(" Voce Acertou!!!");
   }
 
-  function diferente(escolha1,escolha2, x,y,i,j){
-    
+  function diferente(escolha1, escolha2, x, y, i, j) {
+   
+    reset(escolha1, escolha2);
+    desflip(x, y, i, j);
+    erros++;
+  }
+ function tentativas(erros,pontos){
+   if(erros == 2){
+     reset(pontos,erros)
+     erros = 0;
+     pontos = 0;
+   }
+   alert("Voce Perdeu")
+   
+ }
 
-      reset(escolha1,escolha2)
-      desflip(x,y,i,j)
-      erros++
-  
-}
-
- 
 </script>
 
-
 <h1>
-  Pontos:{pontos} 
+  Pontos:{pontos}
 </h1>
 
 <h1>Erros:{erros}</h1>
@@ -186,21 +170,13 @@ cartas = shuffle(cartas)
   {#each estadoTabela.tabela as linha, i}
     <tr>
       {#each linha as dado, j}
-        <td  id={`${i}-${j}`}  on:click={() => flip(i, j)}>
-          
-          <img src={dado} alt="">
+        <td id={`${i}-${j}`} on:click={() => flip(i, j)}>
+          <img src={dado} alt="" />
         </td>
       {/each}
     </tr>
   {/each}
 </table>
-
-
-
-
 <br />
 
-
 <VoltarMenu />
-
-
